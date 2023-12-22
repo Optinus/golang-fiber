0ab1e800a6aaae26199587799626d5f8d9faef84
Optinus <wils2285@gmail.com>
2023-12-22 10:36:25 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON("golang fiber")
+	})
+	app.Listen(":8080")
+}
